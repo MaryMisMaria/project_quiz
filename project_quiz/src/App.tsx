530845122f0e6aaa38bca4edd9e0a22921876236
MaryMisMaria <mariia.mys.mpmpm.2021@lpnu.ua>
2025-01-21 00:18:16 +0200

    commit

---
+++ project_quiz/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Quiz from './components/Quiz';
+
+const App: FC = () => {
+    return (
+        <div>
+            <Quiz />
+        </div>
+    );
+};
+
+export default App;
